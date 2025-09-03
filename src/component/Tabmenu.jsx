1b30f843838a0e23f6bea5d5ee15cc76ac8e13c7
tpflsk495-lab <tpflsk495@gmail.com>
2025-09-03 12:34:48 +0900

    Add files via upload

--- src/component/Tabmenu.jsx
+++ src/component/Tabmenu.jsx
@@ -10,15 +10,15 @@ const Tabmenu = () => {
                 <div className="inner">
                     <h2>HOT! 인기 공연/전시</h2>
                     <div className="tab_btn">
-                        <NavLink to="/Musical" activeClassName="active-link">뮤지컬</NavLink>
+                        <NavLink to="/" activeClassName="active-link">뮤지컬</NavLink>
                         <NavLink to="/Theater" activeClassName="active-link">연극</NavLink>
                         <NavLink to="/Exhibit" activeClassName="active-link">전시/행사</NavLink>
                     </div>
                     <div className="tab_list">
                         <Routes>
-                            <Route path="/Musical" elememt={<Tablist1 />} />
-                            <Route path="/Theater" elememt={<Tablist2 />} />
-                            <Route path="/Exhibit" elememt={<Tablist3 />} />
+                            <Route path="/" element={<Tablist1 />} />
+                            <Route path="/Theater" element={<Tablist2 />} />
+                            <Route path="/Exhibit" element={<Tablist3 />} />
                         </Routes>
                         
                     </div>
